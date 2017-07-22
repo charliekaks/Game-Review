3d06e673796feca910a491d04485e485357f72c1
charliekaks <kaksunchained@gmail.com>
2017-07-22 12:58:36 +0300

    created a one to many relationship between the games and their ratings.

--- app/routes/game.js
+++ app/routes/game.js
@@ -4,12 +4,13 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('game', params.game_id);
   },
-  saveRating(params){
-    var newRating = this.store.createRecord("rating", params);
-    var game = params.game;
-    game.get("ratings").addObject(newRating);
-    newRating.save().then(function(){
-      return
+  actions :{
+    saveRating(params){
+      var newRating = this.store.createRecord("rating", params);
+      var game = params.game;
+      game.get("rating").addObject(newRating);
+      newRating.save().then(function(){
+      return game.save();
     });
-  }
+  }}
 });
